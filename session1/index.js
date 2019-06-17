096306f9fbbff052237dd90dae573d482ca53b76
nguyentadanggiang2k <nguyentadanggiang@gmail.com>
2019-06-17 22:07:17 +0700

    abcxyz

---
+++ session1/index.js
@@ -0,0 +1,15 @@
+// day la dong code 
+// const x = Number(prompt("Enter X"));
+// const y = Number(prompt("Enter Y"));
+// console.log(x+y);
+
+
+
+
+//loop
+
+//comment code
+
+for(let y = 1; y < 101; y++){
+    console.log(y);
+    }
